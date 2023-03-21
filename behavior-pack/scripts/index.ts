30bbbf6fbde8f052cff5123b3d9e123ffb7868cf
Tyler Schrock <tschrock123@gmail.com>
2023-03-21 19:08:12 -0400

    Fix script

--- behavior-pack/scripts/index.ts
+++ behavior-pack/scripts/index.ts
@@ -1,5 +1,5 @@
 import { world } from "@minecraft/server"
 
 world.events.playerSpawn.subscribe(event => {
-    event.player.dimension.runCommand(`say Welcome ${event.player.name}!` )
+    event.player.dimension.runCommandAsync(`say Welcome ${event.player.name}!` )
 })
